6d7ad792fed620c218a39e10a218714cec565699
bhola kumar <33161752+bholach@users.noreply.github.com>
2018-03-06 23:35:16 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const dbURL = "mongodb://localhost:27017/oexam";
+const dbURL = "mongodb://oexam:oexampass@ds255958.mlab.com:55958/oexam";
 module.exports = {
     //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
     database: dbURL,    //dev
